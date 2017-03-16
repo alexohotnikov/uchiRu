887c095f99e7acaadf5eab60adb9367ecb30dded
Alexandr <aleksohotnikov@gmail.com>
2017-03-16 08:31:08 +0300

    'CamelCase' format in Use

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 /*    ---------  All var here!! -------*/
-var one_step = 39.28; // одно деление на спрайте в пикселях
+var oneStep = 39.28; // одно деление на спрайте в пикселях
 // console.group('Start debug...');
-var checkstep = 0;
-var ID_in_an_1 = document.getElementById( 'input-an-1' );
-var ID_in_an_2 = document.getElementById( 'input-an-2' );
-var ID_out_pr_1 = document.getElementById( 'input-pr-1' );
-var ID_out_pr_2 = document.getElementById( 'input-pr-2' );
-var ID_result = document.getElementById( 'result' );
-var ID_arr_1 = document.getElementById( 'ar1' );
-var ID_arr_2 = document.getElementById( 'ar2' );
+var checkStep = 0;
+var idInAn_1 = document.getElementById( 'input-an-1' );
+var idInAn_2 = document.getElementById( 'input-an-2' );
+var idOutPr_1 = document.getElementById( 'input-pr-1' );
+var idOutPr_2 = document.getElementById( 'input-pr-2' );
+var idResult = document.getElementById( 'result' );
+var idArr_1 = document.getElementById( 'ar1' );
+var idArr_2 = document.getElementById( 'ar2' );
 var c = document.getElementById( "myCanvas" );
 var ctx = c.getContext( "2d" );
-var errorenId = '' || undefined;
 /* ------- Рандомом инициализируем сам задачку школьнику -----------  */
 var var_1 = getRandomInt( 6, 9 );
 var sum = getRandomInt( 11, 14 );
 var var_2 = sum - var_1;
-ID_out_pr_1.value = var_1;
-ID_out_pr_2.value = var_2;
+idOutPr_1.value = var_1;
+idOutPr_2.value = var_2;
 document.getElementById( 'result' ).value = '?';
 /* ---------- Functions ------- */
 function getRandomInt( min, max ) {
@@ -44,21 +43,21 @@ function CheckInput( input_varible, prog_varible, an_id, pr_id = false ) {
     setTimeout( function() {
       an_id.classList.remove( 'win-in-an' );
     }, 1000 );
-    if ( checkstep == 0 ) {
-      var ok = ( one_step * var_1 ) + ( one_step * var_2 );
-      ID_arr_2.classList.remove( 'hidden' );
-      ID_in_an_2.hidden = false;
-      ID_in_an_2.style.left = ( ok / 2 );
-      ID_in_an_2.disabled = false;
-      ID_in_an_2.focus();
-      canvasDraw( ok - ( ( one_step * var_2 ) / 2 ), ok, ID_in_an_2, ID_arr_2, one_step * var_1, 50 );
+    if ( checkStep == 0 ) {
+      var ok = ( oneStep * var_1 ) + ( oneStep * var_2 );
+      idArr_2.classList.remove( 'hidden' );
+      idInAn_2.hidden = false;
+      idInAn_2.style.left = ( ok / 2 );
+      idInAn_2.disabled = false;
+      idInAn_2.focus();
+      canvasDraw( ok - ( ( oneStep * var_2 ) / 2 ), ok, idInAn_2, idArr_2, oneStep * var_1, 50 );
     }
-    if ( checkstep == 1 ) {
-      ID_result.disabled = false;
-      ID_result.focus();
-      ID_result.value = "";
+    if ( checkStep == 1 ) {
+      idResult.disabled = false;
+      idResult.focus();
+      idResult.value = "";
     }
-    checkstep++;
+    checkStep++;
     return true;
   } else {
     if ( pr_id != false ) pr_id.classList.add( 'error-pr' );
@@ -70,19 +69,19 @@ function CheckInput( input_varible, prog_varible, an_id, pr_id = false ) {
   }
 }
 /*  ----------------------    */
-canvasDraw( one_step * var_1 / 2, one_step * var_1, ID_in_an_1, ID_arr_1, 0, 0 );
-ID_in_an_1.onkeydown = function( e ) {
+canvasDraw( oneStep * var_1 / 2, oneStep * var_1, idInAn_1, idArr_1, 0, 0 );
+idInAn_1.onkeydown = function( e ) {
   if ( e.keyCode == 13 ) {
-    CheckInput( parseInt( ID_in_an_1.value ), var_1, ID_in_an_1, ID_out_pr_1 );
+    CheckInput( parseInt( idInAn_1.value ), var_1, idInAn_1, idOutPr_1 );
   }
 }
-ID_in_an_2.onkeydown = function( e ) {
+idInAn_2.onkeydown = function( e ) {
   if ( e.keyCode == 13 ) {
-    CheckInput( parseInt( ID_in_an_2.value ), var_2, ID_in_an_2, ID_out_pr_2 );
+    CheckInput( parseInt( idInAn_2.value ), var_2, idInAn_2, idOutPr_2 );
   }
 }
-ID_result.onkeydown = function( e ) {
+idResult.onkeydown = function( e ) {
   if ( e.keyCode == 13 ) {
-    CheckInput( parseInt( ID_result.value ), sum, ID_result, false );
+    CheckInput( parseInt( idResult.value ), sum, idResult, false );
   }
 }
